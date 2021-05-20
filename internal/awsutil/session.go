549e9ac040628af0163b937c04e70b39306697f5
Patrik Egyed <pregnor@cisco.com>
2021-05-20 07:35:32 +0200

    feat(session): added dynamic bucket region option
    Implemented a session option which dynamically
    determines the AWS S3 bucket's region without
    prior static manual setting based on the HEAD
    bucket response.

--- internal/awsutil/session.go
+++ internal/awsutil/session.go
@@ -15,10 +15,14 @@
 package awsutil
 
 import (
+	"net/url"
 	"os"
 
+	"emperror.dev/errors"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 const (
@@ -41,6 +45,68 @@ type SessionOption func(*session.Options)
 func AssumeRoleTokenProvider(provider func() (string, error)) SessionOption {
 	return func(options *session.Options) {
 		options.AssumeRoleTokenProvider = provider
+	}
+}
+
+// DynamicBucketRegion is an option for determining the Helm S3 bucket's AWS
+// region dynamically thus allowing the mixed use of buckets residing in
+// different regions without requiring manual updates on the HELM_S3_REGION,
+// AWS_REGION, or AWS_DEFAULT_REGION environment variables.
+//
+// This HEAD bucket solution works with all kinds of S3 URIs containing
+// the bucket name in the host part.
+//
+// The basic idea behind the HEAD bucket solution and the "official
+// confirmation" this behavior is expected and supported came from a comment on
+// the AWS SDK Go repository:
+// https://github.com/aws/aws-sdk-go/issues/720#issuecomment-243891223
+func DynamicBucketRegion(s3URL string) SessionOption {
+	return func(options *session.Options) {
+		parsedS3URL, err := url.Parse(s3URL)
+		if err != nil {
+			return
+		}
+
+		// Note: The dummy credentials are required in case no other credential
+		// provider is found, but even if the HEAD bucket request fails and
+		// returns a non-200 status code indicating no access to the bucket, the
+		// actual bucket region is returned in a response header.
+		//
+		// Note: A signing region **MUST** be configured, otherwise the signed
+		// request fails. The configured region itself is irrelevant, the
+		// endpoint officially works and returns the bucket region in a response
+		// header regardless of whether the signing region matches the bucket's
+		// region.
+		//
+		// Note: The default S3 endpoint **MUST** be configured to avoid making
+		// the request region specific thus avoiding regional redirect responses
+		// (301 Permanently moved) on HEAD bucket. This setting is only required
+		// because any other region than "us-east-1" would configure a
+		// region-specific endpoint as well, so it's more safe to explicitly
+		// configure the default endpoint.
+		//
+		// Source:
+		// https://github.com/aws/aws-sdk-go/issues/720#issuecomment-243891223
+		configuration := aws.NewConfig().
+			WithCredentials(credentials.NewStaticCredentials("dummy", "dummy", "")).
+			WithRegion("us-east-1").
+			WithEndpoint("s3.amazonaws.com")
+		awsSession := session.Must(session.NewSession())
+		s3Client := s3.New(awsSession, configuration)
+
+		bucketRegionHeader := "X-Amz-Bucket-Region"
+		input := &s3.HeadBucketInput{ // nolint:exhaustivestruct // Note: optional query elements.
+			Bucket: aws.String(parsedS3URL.Host),
+		}
+		awsRequest, _ := s3Client.HeadBucketRequest(input)
+		_ = awsRequest.Send()
+
+		if awsRequest.HTTPResponse == nil ||
+			len(awsRequest.HTTPResponse.Header[bucketRegionHeader]) == 0 {
+			return
+		}
+
+		options.Config.Region = aws.String(awsRequest.HTTPResponse.Header[bucketRegionHeader][0])
 	}
 }
 
